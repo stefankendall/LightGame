679dc37fa44cb8c8d53ff911914048ca75fbd13b
Stefan Kendall <stefankendall@gmail.com>
2015-06-08 21:47:38 -0400

    Refactor

--- LightGame/CourseScene.m
+++ LightGame/CourseScene.m
@@ -21,30 +21,43 @@
 }
 
 - (void)pinch:(UIPinchGestureRecognizer *)pinch {
-    CGFloat scale = pinch.scale;
-    scale = scale > 1 ? 1 : scale;
-    double minZoom = 0.5;
-    scale = (CGFloat) (scale < minZoom ? minZoom : scale);
-    [[self childNodeWithName:@"level"] setScale:scale];
+    BallNode *ball = (BallNode *) [self childNodeWithName:@"//ball"];
+    if (ball.physicsBody.velocity.dy == 0 && ball.physicsBody.velocity.dx == 0) {
+        CGFloat scale = pinch.scale;
+        scale = scale > 1 ? 1 : scale;
+        double minZoom = 0.5;
+        scale = (CGFloat) (scale < minZoom ? minZoom : scale);
+        [[self childNodeWithName:@"level"] setScale:scale];
+    }
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    BallNode *ball = (BallNode *) [self childNodeWithName:@"//ball"];
-    [ball hitInDirection:CGVectorMake(0, 1) withPercentOfMaxForce:1];
+    if ([touches count] == 1) {
+        BallNode *ball = (BallNode *) [self childNodeWithName:@"//ball"];
+        [ball hitInDirection:CGVectorMake(0, 1) withPercentOfMaxForce:1];
+    }
 }
 
 - (void)update:(NSTimeInterval)currentTime {
+    [self followBallWithCamera];
+    [self stopBallIfNecessary];
+}
+
+- (void)stopBallIfNecessary {
+    BallNode *ball = (BallNode *) [self childNodeWithName:@"//ball"];
+    double velocitySquared = pow(ball.physicsBody.velocity.dy, 2) + pow(ball.physicsBody.velocity.dx, 2);
+    if (velocitySquared < 40) {
+        ball.physicsBody.velocity = CGVectorMake(0, 0);
+    }
+}
+
+- (void)followBallWithCamera {
     BallNode *ball = (BallNode *) [self childNodeWithName:@"//ball"];
     Level1Node *level = (Level1Node *) [self childNodeWithName:@"level"];
     double ballYChange = ball.position.y - level.initialBallPosition.y;
     CGPoint newPosition = CGPointMake(level.position.x,
             (CGFloat) (-ballYChange) + [level calculateAccumulatedFrame].size.height / 2);
-    [level runAction:[SKAction moveTo:newPosition duration:0.3]];
-
-    double velocitySquared = pow(ball.physicsBody.velocity.dy, 2) + pow(ball.physicsBody.velocity.dx, 2);
-    if (velocitySquared < 40) {
-        ball.physicsBody.velocity = CGVectorMake(0, 0);
-    }
+    [level runAction:[SKAction moveTo:newPosition duration:0.1]];
 }
 
 
